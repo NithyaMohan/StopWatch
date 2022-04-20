73b5a2812f03b47bdc400c998903eec95b92baf0
Nithya <v.nithiya@gmail.com>
2022-04-20 17:19:48 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input;
+            var Timer = new StopWatch();
+            Console.WriteLine("Enter S to start the Timer and Q to Stop");
+            Input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(Input))
+            {
+                if (string.Equals("s", Input) || string.Equals("S", Input))
+                {
+                    Timer.Start();
+                    Console.WriteLine("Enter Q to Stop");
+                }
+                else if (string.Equals("Q", Input, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Timer.Stop();
+                    Console.WriteLine("Enter S to start the timer");
+                }
+
+                Input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private Boolean _isStart;
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private TimeSpan _spanInterval;
+        public void Start()
+        {
+            if (_isStart)
+            {
+                throw new InvalidOperationException();
+            }
+            _startTime = DateTime.Now;
+            _isStart = true;
+        }
+
+        public string Stop()
+        {
+            if (!_isStart)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _stopTime = DateTime.Now;
+            _spanInterval = _stopTime - _startTime;
+            _isStart = false;
+            return _spanInterval.Seconds.ToString();
+        }
+        
+    }
+}
